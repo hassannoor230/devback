e982d95187bf4dddbe485478d1b26f3f2a0ad093
hassannoor230 <hassannoor2309@gmail.com>
2026-08-28 16:08:23 +0500

    commit

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
-import app from '../server.js'
+import app from './server.js'
 
 export default (req, res) => {
-  if (!req.url.startsWith('/api')) {
-    req.url = `/api${req.url === '/' ? '' : req.url}`
-  }
-
   return app(req, res)
 }
